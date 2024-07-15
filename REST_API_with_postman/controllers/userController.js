1ee68c9a6303bfcdc101f2f3e24a22f925eb3278
AdelMraidi <165065424+AdelMraidi@users.noreply.github.com>
2024-07-15 21:32:12 +0100

    Add files via upload

--- REST_API_with_postman/controllers/userController.js
+++ REST_API_with_postman/controllers/userController.js
@@ -10,18 +10,26 @@ module.exports.getAllusers = async (req, res) => {
 // method : POST
 // @ localhost:8000/user/add
 module.exports.adduser = async (req, res) => {
-  // req.body
-  const { name, email, password, isActive } = req.body;
-  const addeduser = new user({
-    name,
-    // field email : email miniscule
-    email: email.toLowerCase(),
-    password,
-    isActive,
-  });
+  try {
+    // req.body
+    const { name, email, password, isActive } = req.body;
 
-  await addeduser.save();
-  return res.status(200).json(addeduser);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const addeduser = new user({
+      name,
+      // field email : email miniscule
+      email: email.toLowerCase(),
+      password: hashedPassword,
+      isActive,
+    });
+
+    await addeduser.save();
+    return res.status(200).json(addeduser);
+  } catch (e) {
+    return res.status(500).json(e.message);
+  }
 };
 
 // PUT
@@ -39,4 +47,29 @@ module.exports.deleteuser = async (req, res) => {
   const { iddelete } = req.params;
   await user.findByIdAndDelete(iddelete);
   return res.status(200).json("deleted!");
+};
+
+module.exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const loginUser = await user.findOne({ email: email });
+    if (!loginUser) {
+      return res.status(401).send("no user with this email");
+    }
+    const isMatch = await bcrypt.compare(password, loginUser.password);
+    if (!isMatch) {
+      return res.status(400).send("invalid credentials");
+    }
+    const token = jwt.sign(
+      //payload
+      {
+        id: loginUser._id,
+      },
+      "XXXXXXXXX", //jwt_secret
+      { expiresIn: "1h" }
+    );
+    return res.status(200).json({ token });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 };
--- REST_API_with_postman/index.js
+++ REST_API_with_postman/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 require("dotenv").config();
 const { connectDB } = require("./config/database");
-const userRoute = require("./routes/userroute");
+const userRoute = require("./Routes/userRoute");
 
 const server = express();
 
